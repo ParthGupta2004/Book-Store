46d5163e2a2502c5881b2941cfd6ab13cb2a90b7
ParthGupta2004 <119449795+ParthGupta2004@users.noreply.github.com>
2025-05-21 20:12:53 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,159 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<div class="heading">
+   <h3>about us</h3>
+   <p> <a href="home.php">home</a> / about </p>
+</div>
+
+<section class="about">
+
+   <div class="flex">
+
+      <div class="image">
+         <img src="images/about-img.jpg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>"At Bookly, we believe in more than just selling books. We are dedicated to creating a unique shopping experience that nurtures the spirit of the community and celebrates the joy of reading."</p>
+         <p>"Our shelves are stocked with handpicked titles chosen for their quality and relevance. From rare finds to the latest bestsellers, each book is selected with our customers in mind."</p>
+         <a href="contact.php" class="btn">contact us</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+
+   <h1 class="title">client's reviews</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/pic-1.png" alt="">
+         <h3>John</h3>
+         <p>First of all it customer service is excellent. We get all author book . People should try here affordable and best price.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-2.png" alt="">
+         <h3>Marry</h3>
+         <p>Best book store where  books are available for prepartion of exam related or  also other books are available on reasonable price also.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-3.png" alt="">
+         <h3>Raghav</h3>
+         <p>Customer Service is good. Greetings to customer and making the required Books available to Customers is very good.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-4.png" alt="">
+         <h3>Pooja</h3>
+         <p>This book centre have large amount of a books. The engineering study material  are also available.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-5.png" alt="">
+         <h3>Abhinav</h3>
+         <p>I migrated to the online platform on Just books because I was finding it difficult to go to their libraries before closing time.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-6.png" alt="">
+         <h3>Kriti</h3>
+         <p>I love the product because it is very easy to find. The book are in really organized manner you can easily find the book you want.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+      </div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
